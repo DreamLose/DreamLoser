5346eca8855b29fbf09104d2f0f88cc215101a3b
qianfeng <qianfeng@qianfeng-2.local>
2015-11-23 15:01:15 +0800

    v

---
+++ sucess/sucess/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sucess
+//
+//  Created by qianfeng on 15/11/23.
+//  Copyright (c) 2015年 石波. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
